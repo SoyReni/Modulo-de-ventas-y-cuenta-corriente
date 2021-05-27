180467f050e591d21a9c08fbf7fbb886ddff7fb3
Reni <caballeroreni@gmail.com>
2021-05-26 23:30:33 -0400

    Servicios agregado

---
+++ API/Modulo de ventas y cuenta corriente/Modulo de ventas y cuenta corriente/Services/Organizador.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modulo_de_ventas_y_cuenta_corriente.Services
+{
+    public class Organizador
+    {
+    }
+}
