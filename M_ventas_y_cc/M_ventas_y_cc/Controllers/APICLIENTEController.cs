0f654d73181e5bbc04491e329f7d388cc55334fc
DalilaC33 <dalila.castelnovo@fiuni.edu.py>
2021-06-14 14:14:45 -0400

    14/06/2021 controladoresAPI

---
+++ M_ventas_y_cc/M_ventas_y_cc/Controllers/APICLIENTEController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using M_ventas_y_cc.Models;
+
+namespace M_ventas_y_cc.Controllers
+{
+    public class APICLIENTEController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/APICLIENTE
+        public IQueryable<CLIENTE> GetCLIENTE()
+        {
+            return db.CLIENTE;
+        }
+
+        // GET: api/APICLIENTE/5
+        [ResponseType(typeof(CLIENTE))]
+        public IHttpActionResult GetCLIENTE(int id)
+        {
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            if (cLIENTE == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cLIENTE);
+        }
+
+        // PUT: api/APICLIENTE/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCLIENTE(int id, CLIENTE cLIENTE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cLIENTE.CLIENTEId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cLIENTE).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CLIENTEExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/APICLIENTE
+        [ResponseType(typeof(CLIENTE))]
+        public IHttpActionResult PostCLIENTE(CLIENTE cLIENTE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CLIENTE.Add(cLIENTE);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = cLIENTE.CLIENTEId }, cLIENTE);
+        }
+
+        // DELETE: api/APICLIENTE/5
+        [ResponseType(typeof(CLIENTE))]
+        public IHttpActionResult DeleteCLIENTE(int id)
+        {
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            if (cLIENTE == null)
+            {
+                return NotFound();
+            }
+
+            db.CLIENTE.Remove(cLIENTE);
+            db.SaveChanges();
+
+            return Ok(cLIENTE);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CLIENTEExists(int id)
+        {
+            return db.CLIENTE.Count(e => e.CLIENTEId == id) > 0;
+        }
+    }
+}
---
+++ M_ventas_y_cc/M_ventas_y_cc/Controllers/APISTOCKController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using M_ventas_y_cc.Models;
+
+namespace M_ventas_y_cc.Controllers
+{
+    public class APISTOCKController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/APISTOCK
+        public IQueryable<STOCK> GetSTOCK()
+        {
+            return db.STOCK;
+        }
+
+        // GET: api/APISTOCK/5
+        [ResponseType(typeof(STOCK))]
+        public IHttpActionResult GetSTOCK(int id)
+        {
+            STOCK sTOCK = db.STOCK.Find(id);
+            if (sTOCK == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sTOCK);
+        }
+
+        // PUT: api/APISTOCK/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSTOCK(int id, STOCK sTOCK)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sTOCK.STOCKId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sTOCK).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!STOCKExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/APISTOCK
+        [ResponseType(typeof(STOCK))]
+        public IHttpActionResult PostSTOCK(STOCK sTOCK)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.STOCK.Add(sTOCK);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = sTOCK.STOCKId }, sTOCK);
+        }
+
+        // DELETE: api/APISTOCK/5
+        [ResponseType(typeof(STOCK))]
+        public IHttpActionResult DeleteSTOCK(int id)
+        {
+            STOCK sTOCK = db.STOCK.Find(id);
+            if (sTOCK == null)
+            {
+                return NotFound();
+            }
+
+            db.STOCK.Remove(sTOCK);
+            db.SaveChanges();
+
+            return Ok(sTOCK);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool STOCKExists(int id)
+        {
+            return db.STOCK.Count(e => e.STOCKId == id) > 0;
+        }
+    }
+}
